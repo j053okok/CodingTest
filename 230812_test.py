c6daf69f7e9b912356804335bdcb22e521c805d3
j053okok <008youngjoon@naver.com>
2023-08-12 23:10:45 +0900

    test

--- 230812_test.py
+++ 230812_test.py
@@ -1,6 +1,11 @@
-def solution(n, x):
-    return n * x
+def solution(n):
+    x, y = 0, 1
+    a = [1]
+    while len(a) < n:
+        z = x + y
+        a.append(z)
+        x, y = y, z
+    return a
 
 
-print(solution(5, 6))
-#test
+print(solution(10))
